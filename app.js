e72f5fa93e1cfc8116c0c8b3f6fd51e48860b708
jackiezzhen <jackiezhen31@gmail.com>
2023-10-02 09:23:17 -0400

    help

---
+++ app.js
@@ -0,0 +1,41 @@
+/* 
+// declare and define function
+//can pass in arguments or parameters
+function test(x) {
+    console.log(x)
+}
+// run the function
+test("Anything");
+
+//String " "
+const x = "String";
+//declaring a variable, defining a variable
+ test(x);
+
+ //Number/Integer
+ const y = 98;
+ test(y);
+
+ //booleon trade or false
+ const z= true;
+ test(z);
+
+ function add(x, y){
+    console.log(x + y);
+ }
+
+ //need to convert the string into a number
+ let a = Number(prompt("enter a number"));
+ let b =Number(prompt("enter another number"));
+ add(a, b) */
+
+ function add(x,y){
+    return x+y;
+ }
+ const sum = add (45,34);
+ 
+ function calc(){
+    const sum = add (45,34);
+    console.log(sum);
+ }
+ calc();
