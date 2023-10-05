7dd6ddfb581be910354041d2290db12e626852ef
jackiezzhen <jackiezhen31@gmail.com>
2023-10-05 09:25:23 -0400

    fix soemthign quickly

--- app.js
+++ app.js
@@ -44,15 +44,30 @@ const x = "String";
 
 const n = (prompt("Please Provide Your Name"));
 const cm =Number(prompt("Hello, " + n + " please provide your height in CENTIMETERS"));
+console.log("Height of " + cm + " Centimeters");
 const kg =Number(prompt("Hello, " + n + " please provide your height in KILOGRAMS"));
-function value(cm, kg){
-   let bmi = getBMI(cm, kj);
+console.log("Weight of " + kg + " Kilgrams");
+
+function value(){
+   const height= (cm*0.01)
+   const bmi = kg/(height*height)
+   
 }
 
-function compare();
-   if (math()){
-
+function BMI(){
+   if(bmi <= 18.5){
+      console.log(n + ", with a BMI of " + bmi + ", you are underweight");}
+      else if(bmi >= 18.5 && bmi < 24.9){
+         console.log(n + ", with a BMI of " + bmi + ", you are averagely weighted");}     
+      else if (bmi >= 25 && bmi < 30){
+         console.log(n + ", with a BMI of " + bmi + ", you are overweight");}
+   else{
+      console.log(n + ", with a BMI of " + bmi + ", you are obese");
+   }
 }
+value()
+BMI()
+//call function pls remember 
 /*
 function getBMI(x,y){
    return 20;
